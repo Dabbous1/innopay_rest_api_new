69d3208d52cf768935086713498daf15d756ef21
Aly Dabbous <aly.dabbous@innopay.com>
2018-08-26 20:30:46 +0200

    adding address model and migrating address db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_26_182136) do
+ActiveRecord::Schema.define(version: 2018_08_26_182842) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "streetname"
+    t.string "zipcode"
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "receivers", force: :cascade do |t|
     t.string "name"
